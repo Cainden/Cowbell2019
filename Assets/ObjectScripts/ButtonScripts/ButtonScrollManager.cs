30cca14faad9f0b23f941d2b631bfd7e83a1f723
Cainden <45406718+Cainden@users.noreply.github.com>
2020-01-15 13:00:47 -0500

    pushing some bugfixes

--- Assets/ObjectScripts/ButtonScripts/ButtonScrollManager.cs
+++ Assets/ObjectScripts/ButtonScripts/ButtonScrollManager.cs
@@ -16,12 +16,12 @@ public class ButtonScrollManager : MonoBehaviour
         buttons = ButtonScript.CreateButtonArrayFromRoomData(RoomManager.GetAllRoomsofCategory(category, true), button1);
         if (buttons.Length > 4)
         {
-            panel.GetComponent<RectTransform>().offsetMin = new Vector2(panel.GetComponent<RectTransform>().offsetMin.x, 135 - (60 * buttons.Length));
-            scrollView.GetComponentInChildren<Scrollbar>().size = 4 / buttons.Length;
-            scrollView.GetComponentInChildren<Scrollbar>().value = 1;
+            //panel.GetComponent<RectTransform>().offsetMin = new Vector2(panel.GetComponent<RectTransform>().offsetMin.x, 135 - (36 * buttons.Length));
+            //scrollView.GetComponentInChildren<Scrollbar>().size = 4 / buttons.Length;
+            //scrollView.GetComponentInChildren<Scrollbar>().value = 1;
             for (int i = 0; i < buttons.Length; i++)
             {
-                buttons[i].transform.parent = panel.transform;
+                buttons[i].transform.SetParent(panel.transform, false);
             }
             Scrolling = true;
         }
@@ -31,8 +31,6 @@ public class ButtonScrollManager : MonoBehaviour
             Scrolling = false;
         }
     }
-
-
 
 
 }
