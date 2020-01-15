a4185ff95db5b8e488e81e0f0c0362ad8d6a7110
Cainden <45406718+Cainden@users.noreply.github.com>
2020-01-15 17:15:08 -0500

    fixed the bug that caused the hire menu to not appear on the first button click

--- Assets/ObjectScripts/HireDlgScript.cs
+++ Assets/ObjectScripts/HireDlgScript.cs
@@ -19,7 +19,7 @@ public class HireDlgScript : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         SetActive(false);
         b0.gameObject.SetActive(false);
