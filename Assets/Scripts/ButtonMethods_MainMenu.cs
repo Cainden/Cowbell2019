7518e8c3a0e239b90fd5ec2c48aaab5fbcc1a8d0
Dylan D. Rader <dylandarthrader@gmail.com>
2020-01-17 17:31:16 -0500

    Added Main Menu button script

---
+++ Assets/Scripts/ButtonMethods_MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySpace;
+
+public class ButtonMethods_MainMenu : MonoBehaviour
+{
+	public void StartGame()
+	{
+		//AppManager.Ref.ChangeApplicationState(Enums.AppState.Game);
+	}
+
+	public void Quit()
+	{
+		//AppManager.Ref.QuitApplication();
+	}
+}
