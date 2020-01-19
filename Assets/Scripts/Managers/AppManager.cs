24bd68e57b2443c8ad4e2d4fdf3008f95fc59544
Cainden <45406718+Cainden@users.noreply.github.com>
2020-01-19 11:50:04 -0500

    adding app manager and game manager

---
+++ Assets/Scripts/Managers/AppManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySpace;
+
+public class AppManager : MonoBehaviour
+{
+    #region Inspector Variables
+
+    [SerializeField] string GameScene;
+    [SerializeField] string MainMenuScene;
+
+    #endregion
+
+
+    #region Statics
+    public static Enums.AppState CurState;
+
+    //Singleton management.
+    public static AppManager Ref { get; set; }
+    #endregion
+
+    #region Singleton Management
+    private void Awake()
+    {
+        if (!Ref)
+        {
+            Ref = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (Ref != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    #endregion
+
+    public void ChangeApplicationState(Enums.AppState state)
+    {
+        switch (state)
+        {
+            case Enums.AppState.MainMenu:
+                SceneManagement.sceneManagement.LoadScene(MainMenuScene);
+                return;
+            case Enums.AppState.Game:
+                SceneManagement.sceneManagement.LoadScene(GameScene);
+                return;
+            default:
+                break;
+        }
+    }
+
+    public void QuitApplication()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//A Manager of game-loop changing functionality between the tycoon-style daytime gameplay and the nighttime gameplay.
+
+public class GameManager : MonoBehaviour
+{
+
+
+
+
+    //Might not want to do this for the gamemanager?
+    #region Singleton Management
+    
+    public static GameManager Ref { get; set; }
+
+    private void Awake()
+    {
+        if (!Ref)
+        {
+            Ref = this;
+
+            //We don't want this for the gamemanager
+            //DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (Ref != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    #endregion
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            AppManager.Ref.ChangeApplicationState(MySpace.Enums.AppState.MainMenu);
+        }
+    }
+}
