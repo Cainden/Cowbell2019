023648002ac595026c4bf3dfdc9592476ba59045
Cainden <45406718+Cainden@users.noreply.github.com>
2020-01-23 14:44:00 -0500

    adding new manscripts and manscript changes

---
+++ Assets/Scripts/ObjectScripts/ActorScripts/ManScript_Guest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySpace;
+
+public class ManScript_Guest : ManScript
+{
+    public override Enums.ManTypes ManType { get { return Enums.ManTypes.Guest; } }
+}
