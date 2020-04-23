65e108e6272f3720d86ba8e2116591f98dc6d6c9
chillywilson <graphicadam@gmail.com>
2020-04-23 15:00:58 -0400

    Adam_new art
    tweeked ui scale, new art assest, stank particles with a swapping texture shader, atmosphers, elevator start, 3 different moons,  a sun, sky and night with a swapping texture shader, streetlight controler prefab

---
+++ Assets/Scripts/ProtoTyping/streetLights.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class streetLights : MonoBehaviour
+{
+	[Range(0.0f,1f)]
+	public float strtTntesity;
+
+	[SerializeField]
+	List<Light2D> srtLights = new List<Light2D>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		for(int i = 0; i< srtLights.Count; i++)
+		{
+			srtLights[i].intensity = strtTntesity;
+		}
+		
+	}
+}
