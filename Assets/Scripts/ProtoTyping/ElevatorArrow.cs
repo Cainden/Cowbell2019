dc8aa0516326ed9dd2054e18cf7d64c3bb824e1a
chillywilson <graphicadam@gmail.com>
2020-05-07 15:30:16 -0400

    adam
    SomeRooms

---
+++ Assets/Scripts/ProtoTyping/ElevatorArrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorArrow : MonoBehaviour
+{
+	[SerializeField]
+	public int roomNumbers = 1;
+	public float speed;
+
+	Quaternion targetAngle ;
+	public Quaternion currentAngle;
+	// Start is called before the first frame update
+	private void Start()
+	{
+		
+		currentAngle = transform.rotation;
+	}
+
+
+	private void Update()
+	{
+		targetAngle = Quaternion.Euler(270 , 90, 90);
+		transform.rotation = Quaternion.Slerp(currentAngle, targetAngle, Time.time * speed);
+	}
+
+
+}
