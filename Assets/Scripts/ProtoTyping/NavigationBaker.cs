96781095f315b8db2080df6a3fcb6989ef6016f5
chillywilson <graphicadam@gmail.com>
2020-06-08 13:13:58 -0400

    DoorsAndNavMeshes
    Put doors on the hallways and created a navmesh bedrooms

---
+++ Assets/Scripts/ProtoTyping/NavigationBaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class NavigationBaker : MonoBehaviour
+{
+	public List<NavMeshSurface> surfaces = new List<NavMeshSurface>();
+ 
+	public void addToNavmesh(NavMeshSurface a)
+	{
+		
+		surfaces.Add(a);
+		a.BuildNavMesh();
+	}
+}
---
+++ Assets/Scripts/ProtoTyping/SendToNavBaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SendToNavBaker : MonoBehaviour
+{
+	NavigationBaker navBake;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		navBake = GameObject.FindGameObjectWithTag("NavBaker").GetComponent<NavigationBaker>();
+		navBake.addToNavmesh(this.gameObject.GetComponent<NavMeshSurface>());
+
+
+
+    }
+
+ 
+}
