bdc7b315fbc5ee3b28e5c3bf1577e741d0a4c09e
IceLeviathan <67759597+IceLeviathan@users.noreply.github.com>
2020-10-06 10:36:54 -0700

    Add white plus and update card with script and plus
    Add white plus so that we can use additive color to get an exact color. Also add the tarot card script so we can fill in the info on the cards.

---
+++ Assets/Scripts/ObjectScripts/UIScripts/TarotCardInfoScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TarotCardInfoScript: MonoBehaviour
+{
+    public GameObject CharacterHead;
+    private SpriteRenderer HeadSprite;
+    public GameObject CharacterBody;
+    private SpriteRenderer BodySprite;
+
+    public GameObject BountyInfo;
+    public GameObject LocationInfo;
+    private TextMeshProUGUI BountyText;
+    private TextMeshProUGUI LocationText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Character Art set up
+        HeadSprite = CharacterHead.GetComponent<SpriteRenderer>();
+        BodySprite = CharacterBody.GetComponent<SpriteRenderer>();
+        //Character Info set up
+        BountyText = BountyInfo.GetComponent<TextMeshProUGUI>();
+        LocationText = LocationInfo.GetComponent<TextMeshProUGUI>();
+    }
+
+    void TerotCardInfo(Sprite FillHeadSprite, Sprite FillBodySprite, string FillBountyText, string FillLocationText)
+    {
+        HeadSprite.sprite = FillHeadSprite;
+        BodySprite.sprite = FillBodySprite;
+        BountyText.text = FillBountyText;
+        LocationText.text = FillLocationText;
+    }
+}
