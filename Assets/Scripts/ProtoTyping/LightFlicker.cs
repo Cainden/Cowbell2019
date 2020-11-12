ab7599ed1d23c7fcde455676b1f3bfb01e54199d
chillywilson <graphicadam@gmail.com>
2020-11-11 20:42:08 -0500

    new card table
    card table scene

---
+++ Assets/Scripts/ProtoTyping/LightFlicker.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+	[Range(0.0f, 10f)]
+	public float strtTntesity;
+	[Range(0.0f, 10f)]
+	public float maxIntensity;
+	public int smoothing = 5;
+
+	[SerializeField]
+	List<Light> candles = new List<Light>();
+
+	Queue<float> smoothQueue;
+	float lastSum = 0;
+
+
+
+	// Start is called before the first frame update
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		smoothQueue = new Queue<float>(smoothing);
+
+		for (int i = 0; i < candles.Count; i++)
+		{
+			candles[i].intensity = strtTntesity;
+			
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+	{
+		while (smoothQueue.Count >= smoothing)
+		{
+			lastSum -= smoothQueue.Dequeue();
+		}
+
+		for (int i = 0; i < candles.Count; i++)
+		{
+			float newVal = Random.Range(strtTntesity, maxIntensity);
+			smoothQueue.Enqueue(newVal);
+			lastSum += newVal;
+			candles[i].intensity = lastSum / (float)smoothQueue.Count; ;
+			Debug.Log("candle:" + candles[i].intensity);
+		}
+
+	}
+
+	/// <summary>
+	/// Reset the randomness and start again. You usually don't need to call
+	/// this, deactivating/reactivating is usually fine but if you want a strict
+	/// restart you can do.
+	/// </summary>
+	public void Reset()
+	{
+		smoothQueue.Clear();
+		lastSum = 0;
+	}
+
+
+
+
+
+
+
+}
