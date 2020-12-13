e9cffc6614e484e43137d2fa759654f905b4fb92
chillywilson <graphicadam@gmail.com>
2020-12-13 13:07:49 -0500

    new clock
    put in new clock ui and monster clock UI

--- Assets/Scripts/ProtoTyping/Clock_display.cs
+++ Assets/Scripts/ProtoTyping/Clock_display.cs
@@ -1,22 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Clock_display : MonoBehaviour
 {
+	public Image MainClock;
+	public Image moneyExtention;
+	public Sprite mainClockDay;	
+	public Sprite mainClockMONSTER;
+	public Sprite dayExtention;
+	public Sprite nightExtention;
+	public bool switchImage;
+
 	public TimeManager timetrack;
 	public string displaytime;
 	public GameObject dial;
 	public float dialRot;
 	public GameObject hourTurner;
 	public GameObject minutTurner;
+	
+
 
 	private void Start()
 	{
+		switchImage = true;
 		//dialRot = 4;
+		MainClock.sprite = mainClockDay;
+		moneyExtention.sprite = dayExtention;
+
 	}
 	private void Update()
 	{
+		if(switchImage == true)
+		{
+			MainClock.sprite = mainClockDay;
+			moneyExtention.sprite = dayExtention;
+		}
+
+		else
+		{
+			MainClock.sprite = mainClockMONSTER;
+			moneyExtention.sprite = nightExtention;
+		}
+
 		// Take the 0 to 1 clock ratio and turn it into degrees
 		dialRot = TimeManager.RatioCycleTime * 360 ;
 		// do unity euler angles because it's stupid
@@ -31,7 +58,10 @@ public class Clock_display : MonoBehaviour
 		minutTurner.transform.rotation = Quaternion.Euler(v.x, v.y, (v.z * -1)*12);
 	}
 
-	
+	 public void flipImage()
+	{
+		switchImage = !switchImage;
+	}
 
 
 
