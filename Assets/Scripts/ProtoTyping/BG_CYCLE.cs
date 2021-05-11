3393e4b21fcec5fb1a79d8a1b84da058cdc81fc7
chillywilson <graphicadam@gmail.com>
2021-05-11 14:01:43 -0400

    day, background swap
    background swaper

---
+++ Assets/Scripts/ProtoTyping/BG_CYCLE.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_CYCLE : MonoBehaviour
+{
+	public TimeManager timetrack;
+	public Material bgMAT;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		bgMAT.SetFloat("trans", TimeManager.RatioCycleTime);
+		Debug.Log(TimeManager.RatioCycleTime);
+    }
+}
