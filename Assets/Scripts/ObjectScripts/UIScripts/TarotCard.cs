6421ccad3e0c7a11f81e042a9878c31d6bfe9ad9
Chase Cobb <chase@8bitroots.com>
2021-05-13 14:47:37 -0400

    Fixed TarotCard exceptions, renamed and encapsulated members

--- Assets/Scripts/ObjectScripts/UIScripts/TarotCard.cs
+++ Assets/Scripts/ObjectScripts/UIScripts/TarotCard.cs
@@ -6,63 +6,100 @@ using TMPro;
 [RequireComponent(typeof(Animator))]
 public class TarotCard : MonoBehaviour
 {
-    #region Inspector Variables
-    public GameObject NameInfo;
-    private TextMeshProUGUI NameText;
-    public GameObject CharacterHead;
-    private SpriteRenderer HeadSprite;
-    public GameObject CharacterBody;
-    private SpriteRenderer BodySprite;
+    private static readonly string CHAR_NAME = "CharacterName";
+    private static readonly string CHAR_HEAD_ART = "CharacterHeadArt";
+    private static readonly string CHAR_BODY_ART = "CharacterBodyArt";
+    private static readonly string BOUNTY = "Tarrot_Hobbies";
 
-    public GameObject BountyInfo;
-    private TextMeshProUGUI BountyText;
-    public GameObject LocationInfo;
-    private TextMeshProUGUI LocationText;
+    private GameObject m_nameInfo;
+    private TextMeshPro m_nameText;
+    private GameObject m_characterHead;
+    private SpriteRenderer m_headSprite;
+    private GameObject m_characterBody;
+    private SpriteRenderer m_bodySprite;
 
-    private Animator CardAnimator;
-    private bool flipping;
-    #endregion
+    private GameObject m_bountyInfo;
+    private TextMeshPro m_bountyText;
 
-    private void Update()
-    {
-		
-    }
+    private Animator m_cardAnimator;
+    private bool m_flipping;
 
     // Start is called before the first frame update
     void Start()
     {
-        CardAnimator = GetComponent<Animator>();
-        flipping = false;
-    }
+        FindChildObjectByName(CHAR_NAME, transform, ref m_nameInfo);
+        m_nameText = m_nameInfo.GetComponent<TextMeshPro>();
 
-    void Awake()
-    {
-        NameText = NameInfo.GetComponent<TextMeshProUGUI>();
+
         //Character Art set up
-        HeadSprite = CharacterHead.GetComponent<SpriteRenderer>();
-        BodySprite = CharacterBody.GetComponent<SpriteRenderer>();
+        FindChildObjectByName(CHAR_HEAD_ART, transform, ref m_characterHead);
+        m_headSprite = m_characterHead.GetComponent<SpriteRenderer>();
+        FindChildObjectByName(CHAR_BODY_ART, transform, ref m_characterBody);
+        m_bodySprite = m_characterBody.GetComponent<SpriteRenderer>();
+
         //Character Info set up
-        BountyText = BountyInfo.GetComponent<TextMeshProUGUI>();
-        LocationText = LocationInfo.GetComponent<TextMeshProUGUI>();
+        FindChildObjectByName(BOUNTY, transform, ref m_bountyInfo);
+        m_bountyText = m_bountyInfo.GetComponent<TextMeshPro>();
+        //LocationText = LocationInfo.GetComponent<TextMeshProUGUI>();
+        m_cardAnimator = GetComponent<Animator>();
+        m_flipping = false;
+
+        // TODO : REMOVE THIS!!
+        QuickTest();
     }
 
-    public void TarotCardInfo(string FillNameText, Sprite FillHeadSprite, Sprite FillBodySprite, string FillBountyText, string FillLocationText)
+    private void QuickTest()
     {
-        NameText.text = FillNameText;
-        HeadSprite.sprite = FillHeadSprite;
-        BodySprite.sprite = FillBodySprite;
-        BountyText.text = FillBountyText;
-        LocationText.text = FillLocationText;
+        SetName("Zavala");
+        SetBountyText("Defeating the Darkness!!");
+    }
+
+    private void FindChildObjectByName(string name, Transform transformObject, ref GameObject targetObject)
+    {
+        if(transformObject.gameObject.name == name)
+        {
+            targetObject = transformObject.gameObject;
+        }
+
+        if (targetObject == null)
+        {
+            foreach (Transform child in transformObject)
+            {
+                FindChildObjectByName(name, child, ref targetObject);
+
+                if (targetObject != null && targetObject.name == name)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    public void TarotCardInfo(string FillNameText, Sprite FillHeadSprite, Sprite FillBodySprite, string FillBountyText)
+    {
+        m_nameText.text = FillNameText;
+        m_headSprite.sprite = FillHeadSprite;
+        m_bodySprite.sprite = FillBodySprite;
+        m_bountyText.text = FillBountyText;
+    }
+
+    public void SetName(string name)
+    {
+        m_nameText.text = name;
+    }
+
+    public void SetBountyText(string bountyText)
+    {
+        m_bountyText.text = bountyText;
     }
 
     public void Flip()
     {
-        if (CardAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        if (m_cardAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
         {
-            flipping = true;
-            CardAnimator.SetBool("CardRevealed", !CardAnimator.GetBool("CardRevealed"));
-            CardAnimator.SetTrigger("FlipCard");
-            Debug.Log("Flipping a card!");
+            m_flipping = true;
+            m_cardAnimator.SetBool("CardRevealed", !m_cardAnimator.GetBool("CardRevealed"));
+            m_cardAnimator.SetTrigger("FlipCard");
         }
     }
 }
