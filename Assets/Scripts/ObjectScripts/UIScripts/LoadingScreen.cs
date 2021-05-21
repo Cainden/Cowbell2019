04571e86af9a3f6f17be8a58579a5827ea2398dc
Chase Cobb <chase@8bitroots.com>
2021-05-21 12:22:51 -0400

    Loading screen implemented into SceneManager

--- Assets/Scripts/ObjectScripts/UIScripts/LoadingScreen.cs
+++ Assets/Scripts/ObjectScripts/UIScripts/LoadingScreen.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class LoadingScreen : MonoBehaviour
 {
+    private Camera m_camera;
     void Awake()
     {
         DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        ShowLoadingScreen(false);
+    }
+
+    /// <summary>
+    /// Show/Hide the loading screen.
+    /// </summary>
+    /// <param name="shouldShow">True to show the screen. False to hide it.</param>
+    public void ShowLoadingScreen(bool shouldShow)
+    {
+        if(m_camera == null)
+        {
+            m_camera = GetComponent<Camera>();
+        }
+
+        m_camera.enabled = shouldShow;
+        transform.GetChild(0).gameObject.SetActive(shouldShow);
     }
 }
